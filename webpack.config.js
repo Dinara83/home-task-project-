48e69ea8004bfcb21744bd2d2c9aece3d31b0ee9
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-12-27 00:06:59 +0200

    доп.настройки

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 const path = require('path');
+const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const WebpackBar = require('webpackbar');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
+module.exports = env =>
+  webpackMerge({
+    mode: env.mode,
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'src/index.html' }),
+      new MiniCssExtractPlugin({ filename: 'common.css' }),
+      new CleanWebpackPlugin(),
+      new FriendlyErrorsWebpackPlugin(),
+      new WebpackBar(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new MiniCssExtractPlugin({ filename: 'common.css' }),
-    new CleanWebpackPlugin(),
-  ],
-  devServer: {
-    port: 9000,
-    open: true,
-  },
-};
+    devServer: {
+      port: 9000,
+      open: true,
+    },
+  });
